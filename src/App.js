1464002659d2c82963e7101a908b876837a4af58
spshah <spshah@paypal.com>
2020-05-28 00:29:15 -0700

    Section-4: Setup directory structure, homepage and background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from '../src/pages/homepage/homepage.component'
 
 function App() {
   return (
     <div>
-      Hello World!
+      <HomePage/>
     </div>
   );
 }
